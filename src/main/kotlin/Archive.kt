a6435dcb39348a08088944f83f7cd3b7a1a4d2e0
Myp3uk <l89528256664@yandex.ru>
2023-09-23 00:11:30 +0300

    1

---
+++ src/main/kotlin/Archive.kt
@@ -0,0 +1,25 @@
+class Archive(val name: String) {
+    val notes = mutableListOf<String>()
+
+    fun addNote() {
+        println("Введите текст заметки:")
+        val note = readLine() ?: ""
+        if (note.isNotEmpty()) {
+            notes.add(note)
+            println("Заметка успешно добавлена")
+        } else {
+            println("Заметка не может быть пустой")
+        }
+    }
+
+    fun showNotes() {
+        if (notes.isNotEmpty()) {
+            println("Список заметок:")
+            for (i in notes.indices) {
+                println("${i + 1}. ${notes[i]}")
+            }
+        } else {
+            println("Архив пуст")
+        }
+    }
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,84 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
+    val archives = mutableListOf<Archive>()
+    val menuHelper = MenuHelper(listOf("Выбор архива", "Создание архива", "Выбор заметки", "Создание заметки", "Экран заметки"))
+
+    while (true) {
+        println("Меню:")
+        menuHelper.showMenu()
+
+        when (menuHelper.readInput()) {
+            1 -> {
+                if (archives.isNotEmpty()) {
+                    println("Выберите архив:")
+                    for (i in archives.indices) {
+                        println("${i + 1}. ${archives[i].name}")
+                    }
+                    val archiveIndex = menuHelper.readInput() - 1
+                    if (archiveIndex in archives.indices) {
+                        val archive = archives[archiveIndex]
+                        archive.showNotes()
+                    } else {
+                        println("Некорректный выбор архива")
+                    }
+                } else {
+                    println("Архивы отсутствуют")
+                }
+            }
+            2 -> {
+                println("Введите название архива:")
+                val archiveName = readLine() ?: ""
+                if (archiveName.isNotEmpty()) {
+                    archives.add(Archive(archiveName))
+                    println("Архив успешно создан")
+                } else {
+                    println("Название архива не может быть пустым")
+                }
+            }
+            3 -> {
+                if (archives.isNotEmpty()) {
+                    println("Выберите архив:")
+                    for (i in archives.indices) {
+                        println("${i + 1}. ${archives[i].name}")
+                    }
+                    val archiveIndex = menuHelper.readInput() - 1
+                    if (archiveIndex in archives.indices) {
+                        val archive = archives[archiveIndex]
+                        archive.addNote()
+                    } else {
+                        println("Некорректный выбор архива")
+                    }
+                } else {
+                    println("Архивы отсутствуют")
+                }
+            }
+            4 -> {
+                if (archives.isNotEmpty()) {
+                    println("Выберите архив:")
+                    for (i in archives.indices) {
+                        println("${i + 1}. ${archives[i].name}")
+                    }
+                    val archiveIndex = menuHelper.readInput() - 1
+                    if (archiveIndex in archives.indices) {
+                        val archive = archives[archiveIndex]
+                        archive.showNotes()
+                        println("Выберите заметку:")
+                        val noteIndex = menuHelper.readInput() - 1
+                        if (noteIndex in archive.notes.indices) {
+                            println("Текст заметки: ${archive.notes[noteIndex]}")
+                        } else {
+                            println("Некорректный выбор заметки")
+                        }
+                    } else {
+                        println("Некорректный выбор архива")
+                    }
+                } else {
+                    println("Архивы отсутствуют")
+                }
+            }
+            else -> {
+                println("Выход из программы")
+                return
+            }
+        }
+    }
 }
